0f0c8dd68ac153d5e475fbf5de486cc7b4588150
Eugenio <eugenionlopez@gmail.com>
2017-01-01 17:50:41 -0300

    About triangles

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,35 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  parameters = [a, b, c]
+  uniques = parameters.uniq.length
+
+  if parameters.select {|value| value < 1 }.length > 0
+    raise TriangleError.new("Invalid Triangle Values: #{parameters.select {|value| value < 1}}")
+  end
+
+  if uniques == 1
+    sides = parameters.reduce(&:+)
+  else
+    if parameters.count(parameters.max) > 1
+      sides = parameters.select { |val| val == parameters.max }.reduce(&:+)
+    else
+      sides = parameters.select { |val| val < parameters.max }.reduce(&:+)
+    end
+  end
+
+  if parameters.max >= sides
+    raise TriangleError.new("Invalid triangle size")
+  end
+
+  case uniques
+  when 1
+    return :equilateral
+  when 2
+    return :isosceles
+  when 3
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
